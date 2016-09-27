fdf04ce437f2b9e2c3ab0b26e36494fdc6145eba
Isaque de Araujo <isaquearaujo22@gmail.com>
2016-09-27 13:25:09 -0300

    Added AdMob SDK

---
+++ Assets/Scripts/AdsController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class AdsController : MonoBehaviour {
+	string adUnitId = "INSERT_ANDROID_BANNER_AD_UNIT_ID_HERE";
+	BannerView banner;
+	// Use this for initialization
+	void Start () {
+		banner = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+		// Create an empty ad request.
+		AdRequest request = new AdRequest.Builder().Build();
+		// Load the banner with the request.
+		banner.LoadAd(request);
+	}
+	
+	// Update is called once per frame
+	void OnDestroy () {
+		if (banner != null)
+			banner.Hide ();
+	}
+}
