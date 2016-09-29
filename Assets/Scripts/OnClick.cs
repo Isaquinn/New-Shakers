b6657a17d3b91a9e7a8adcc1f86b671ab1db16ce
Isaque de Araujo <isaquearaujo22@gmail.com>
2016-09-28 22:27:36 -0300

    Add click buttons
    play button, button to confirm instrument and button to change
    instrument.

---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void Play()
+	{
+		Application.LoadLevel ("select");
+		Debug.Log ("Play");
+	}
+	public void SelectInstrument()
+	{
+		Application.LoadLevel ("app");
+		Debug.Log ("App");
+	}
+	public void ChangeInstrument()
+	{
+		Application.LoadLevel ("select");
+		Debug.Log ("Select");
+	}
+}
