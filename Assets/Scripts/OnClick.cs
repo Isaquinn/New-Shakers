90afc6230b7c8b28df8b41b0be666e05b7830632
Isaque de Araujo <isaquearaujo22@gmail.com>
2016-09-29 22:40:54 -0300

    Change instruments

--- Assets/Scripts/OnClick.cs
+++ Assets/Scripts/OnClick.cs
@@ -3,16 +3,16 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class OnClick : MonoBehaviour {
-
+	private int imageselect;
+	private GameObject imageinstrument;
 	// Use this for initialization
 	void Start () {
+		//imageselect = 0;
+		imageinstrument = GameObject.Find("Canvas/Image");
 	
 	}
-	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+
 	public void Play()
 	{
 		Application.LoadLevel ("select");
@@ -27,5 +27,43 @@ public class OnClick : MonoBehaviour {
 	{
 		Application.LoadLevel ("select");
 		Debug.Log ("Select");
+	}
+	public void UpButton()
+	{
+		imageselect -= 1;
+	}
+	public void DownButton()
+	{
+
+		imageselect += 1;
+		Debug.Log (imageselect);
+	}
+	void Update () 
+	{
+		if(imageselect == 1)
+		{
+			imageinstrument.gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("1");
+		}
+		else if(imageselect == 2)
+		{
+			imageinstrument.gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("Claves");
+		}
+		else if(imageselect == 3)
+		{
+			imageinstrument.gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("guiro");
+		}
+		else if(imageselect == 0)
+		{
+			imageinstrument.gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("0");
+
+		}
+		if(imageselect > 3)
+		{
+			imageselect = 0;
+		}
+		 if(imageselect < 0)
+		{
+			imageselect = 3;
+		}
 	}
 }
